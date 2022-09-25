75c3fd080f8e1b0ccc4e79390639073be7fe03e2
Hyeon <leemh3005@gmail.com>
2022-09-26 02:08:55 +0900

    2진수, 10진수, 16진수

--- Program.cs
+++ Program.cs
@@ -10,14 +10,23 @@ namespace CSharp
         {
             // byte(1바이트 0~255), short(2바이트-3만~3만), int(4바이트 -21억~21억), long(8바이트)
             // sbyte(-128~127), ushort(0~6만), uint(0~43억), ulong
-            int hp;
-            short level = 100;
-            long id;
+            
+            // 10진수
+            // 00 01 02 03 04 05 06 07 08 09 
+                // 10
+            
+            // 2진수
+            // 0~1
+                // 0b00 0b01 0b10 0b11 0b100
+                // 0b10001111 = 0x8F
 
-            hp = 100;
+            // 16진수
+            // 0~9 a b c d e f
+                // 0x00 0x01 0x02 0x03 . . 0x0f
+                    //0x10
 
 
-            Console.WriteLine("Hello Number ! {0}", hp);
+            //Console.WriteLine("Hello Number ! {0}", hp);
         }
     }
 }
