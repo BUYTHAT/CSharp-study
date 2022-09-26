b482ff4dd4c30aa1aef6ff2272c8f63c0031b9de
Hyeon <leemh3005@gmail.com>
2022-09-26 20:10:57 +0900

    데이터 마무리

--- Program.cs
+++ Program.cs
@@ -6,21 +6,13 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            int hp = 100;
-            int level = 50;
+            var num = 3;
 
-            bool isAlive = (hp > 0);
-            bool isHighLevel = (level >= 40);
+            var num2 = "Hello World";
 
-            // && AND      || OR      ! NOT
-            // a = 살아있는 고렙 유저인가요?
-            bool a = isAlive && isHighLevel;
+            // var 남용 X   읽는사람 입장에서 불편할 수 있음
 
-            // b = 살아있거나, 고렙 유저이거나, 둘 중 하나인가요?
-            bool b = isAlive || isHighLevel;
 
-            // c = 죽은 유저인가요?
-            bool c = !isAlive; 
         }
     }
 }
