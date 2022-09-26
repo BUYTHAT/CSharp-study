e65ba7731e7049443409c95e66947dcd02482974
Hyeon <leemh3005@gmail.com>
2022-09-26 21:18:45 +0900

    switch

--- Program.cs
+++ Program.cs
@@ -7,32 +7,41 @@ namespace CSharp
         static void Main(string[] args)
         {
 
-            /*
-            int hp = 100;
-            bool isDead = (hp <= 0);
+            /*int choice = 1; // 0:가위 1:바위 2:보 3:치트키
 
-            if (isDead){
-                Console.WriteLine("You are dead!");
-            }
-            else{
-                Console.WriteLine("You are alive!");
-            }
-            */
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("가위입니다.");
+                    break;
+                case 1:
+                    Console.WriteLine("바위입니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("보입니다.");
+                    break;
+                case 3:
+                    Console.WriteLine("치트키입니다.");
+                    break;
+                default:
+                    Console.WriteLine("다 실패 했습니다.");
+                    break;
+            }*/
 
-            int choice = 0; // 0:가위 1:바위 2:보 3:치트키
 
-            if (choice == 0)
-            {
-                Console.WriteLine("가위입니다.");
-            } else if (choice == 1)
-            {
-                Console.WriteLine("바위입니다.");
-            } else if (choice == 2)
-            {
-                Console.WriteLine("보입니다.");
-            } else {
-                Console.WriteLine("치트키입니다.");
-            }
+            // 삼항연산자
+
+            int number = 25;
+
+            //bool isPair = (조건 ? 맞을때 : 틀릴때);
+            bool isPair = ((number & 2) == 0 ? true : false);
+
+            if( (number & 2) == 0 )
+                isPair = true;
+            else
+                isPair = false;
+
+            
 
 
         }
--- Session-2.md
+++ Session-2.md
@@ -9,3 +9,29 @@ if (참 또는 거짓의 조건문)
 } else {
 	*위 조건문에 대하여 참이 아닌경우  실행*
 }
+
+# switch
+
+switch (choice)
+{
+    case 0:
+        Console.WriteLine("가위입니다.");
+        break;
+    case 1:
+        Console.WriteLine("바위입니다.");
+        break;
+    case 2:
+        Console.WriteLine("보입니다.");
+        break;
+    case 3:
+        Console.WriteLine("치트키입니다.");
+        break;
+    default:
+        Console.WriteLine("다 실패 했습니다.");
+        break;
+}
+
+삼항연산자
+
+bool isPair = ( 조건 ? 맞을때 : 틀릴때 );
+ex)bool isPair = ((number & 2) == 0 ? true : false);
