843c3ee4d9d436dc3877b2fd3b460f401763d234
Hyeon <leemh3005@gmail.com>
2022-09-27 15:26:22 +0900

    연습문제

--- Program.cs
+++ Program.cs
@@ -1,26 +1,65 @@
 ﻿using System;
-using System.Net.Security;
+using System.Dynamic;
 
 namespace CSharp
 {
     class Program
     {
-        // 덧셈 함수
-        static void AddOne(ref int number)
+        static int Factorial(int n)
         {
-            number = number + 1; 
+            int result = 1;
+
+            for (int i = n; i > 0; i--)
+            {
+                result *= i;
+            }
+            return result;
         }
 
         static void Main(string[] args)
         {
+            /*
+            //구구단
+            int a;
+            int result;
 
-            //복사 (짭) 참조 (찐)
-            int a = 10;
+            for (a = 2; a < 10; a++)
+            {
+                for (int i = 1; i < 10; i++)
+                {
 
-            Program.AddOne(ref a); 
-
-            Console.WriteLine(a);
+                    result = a * i;
+                    Console.WriteLine($"{a} * {i} = {result}");
+                } 
+            }
             
+            //별 피라미드
+            int StarNum = 1;
+
+            for (int i = 2; i < 7; i++)
+            {
+                while (StarNum > 0)
+                {
+                    Console.Write("*");
+                    StarNum--;
+                }
+                Console.WriteLine();
+                StarNum = StarNum + i; 
+;
+            }*/
+            
+
+            //팩토리얼구현
+            // 5! = 5 * 4 * 3 * 2 * 1
+
+            int ret = Factorial(5);
+            Console.WriteLine(ret);
+
+            
+
+
+
+
         }
     }
 }
