b01cf7fdd7b83166428ca5f032b8d63547d5eeca
Hyeon <leemh3005@gmail.com>
2022-09-27 23:23:46 +0900

    TextRPG 플레이어 생성

--- Program.cs
+++ Program.cs
@@ -12,6 +12,13 @@ namespace CSharp
             Mage = 3,
         }
 
+        struct Player
+        {
+            public int hp;
+            public int attack;
+            
+        }
+
         static ClassType ChooseClass()
         {
 
@@ -39,6 +46,33 @@ namespace CSharp
             return choice;
         }
 
+        static void CreatePlayer(ClassType choice, out Player player)
+        {
+            
+
+            switch (choice)
+            {
+                case ClassType.Knight:
+                    player.hp = 100;
+                    player.attack = 10;
+                    break;
+                case ClassType.Archer:
+                    player.hp = 75;
+                    player.attack = 12;
+                    break;
+                case ClassType.Mage:
+                    player.hp = 50;
+                    player.attack = 15;
+                    break;
+                default:
+                    player.hp = 0;
+                    player.attack = 0;
+                    break;
+
+            }
+        }
+
+
 
         static void Main(string[] args)
         {
@@ -50,27 +84,34 @@ namespace CSharp
             {
                 ClassType choice = ChooseClass();
                 if (choice != ClassType.None)
+                {
+
+                    Player player;
+                   
+
+                    CreatePlayer(choice, out player);
+
+                    switch (choice)
+                    {
+                        case ClassType.Knight:
+                            Console.Write("기사");
+                            break;
+                        case ClassType.Archer:
+                            Console.Write("궁수");
+                            break;
+                        case ClassType.Mage:
+                            Console.Write("법사");
+                            break;
+
+                    }
+
+                    Console.Write($" HP : {player.hp} / ATTACK : {player.attack}");
+
                     break;
+                    
+                }
+                    
             }
-
-
-
-
-            //내가 한것
-            /*
-            int JobChoice;
-
-            do
-            {
-                Console.WriteLine("직업을 선택해주세요!");
-                Console.WriteLine("[1] 기사");
-                Console.WriteLine("[2] 궁수");
-                Console.WriteLine("[3] 법사");
-
-                JobChoice = Convert.ToInt32(Console.ReadLine());
-
-            } while (JobChoice != 1 && JobChoice != 2 && JobChoice != 3);
-            */
 
         }
 
--- TextRPG.md
+++ TextRPG.md
@@ -1,1 +1,40 @@
 # TextRPG
+
+TextRPG 프로젝트를 하면서 몰랐던, 또는 기억했으면 좋을 문법들을 정리
+
+
+Consol.Write 에서 string 속에 int 변수를 넣고싶을때
+""앞에 $를 두고 {변수이름} 으로 값 호출 가능
+
+```
+Console.Write($"HP : {hp} / ATTACK : {attack}");
+```
+
+####ClassType
+
+`int choice;` 를 하면 'int' 타입의 변수를 할당해서 ''choice'라는 이름으로 코드에서 활용하는데,
+마찬가지로 `ClassType choice;`를 하면 'ClassType' 타입의 변수를 할당하게됨
+
+```
+enum ClassType
+{
+	None = 0,
+	Knight = 1,
+	Archer = 2,
+	Mage = 3
+}
+```
+
+ClassType은 enum 타입이기 때문에 위 4개의 값 중 하나를 골라서 사용함
+메모리 상으로는 일반 정수처럼 1, 2, 3 혹은 4에 불과하지만,
+C# 코드에선 1, 2, 3 이라는 의미없는 값을 하드코딩해서 넣는 것이 아니라
+ClassType.Knight와 같이 우리가 알아보기 쉬운 값을 사용할 수 있게 됨
+
+
+#### 구조체, struct
+
+```
+ClassType은 enum 타입이기 때문에 위 4개의 값 중 하나를 골라서 사용하게 됩니다.
+메모리 상으로는 일반 정수처럼 1, 2, 3 혹은 4에 불과하지만,
+C# 코드에선 1, 2, 3 이라는 의미없는 값을 하드코딩해서 넣는 것이 아니라
+ClassType.Knight와 같이 우리가 알아보기 쉬운 값을 사용할 수 있게 됩니다.
