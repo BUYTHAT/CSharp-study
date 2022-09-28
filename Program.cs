60b5ef9683afdeb8df8f23346ccac51ba99e3aac
Hyeon <leemh3005@gmail.com>
2022-09-28 22:56:40 +0900

    객체지향

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿class Knight
+{
+    static int hp = 10;
+    static int attack = 200;
+
+    Knight(int hp, int attack)
+    {
+        
+    }
+
+    static public int Test()
+    {
+        return hp + attack;
+    }
+}
+
+Knight knight = new Knight(130, 2)
+    knight.Test()
