132e939dc258e361f7a8ee05bbd2a704e37071ad
Hyeon <leemh3005@gmail.com>
2022-09-30 17:45:16 +0900

    List

--- Program.cs
+++ Program.cs
@@ -6,21 +6,29 @@ namespace CSharp
     {
         static void Main(string[] agrs)
         {
-            int[,] map = new int[2, 3];
+            int[] arr = new int[10];
 
+            //list <- 동적 배열 [ 0, 1, 2, 3, 4 ] 
+            List<int> list = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(i);
+            }
 
-            // [  ,   ,   ,   ]
-            // [  ,   ,   ,   ,   ,   ,   ]
-            // [  ,   ,   ,   ,   ]
+            //삽입 삭제
+            list.Insert(2, 999);
 
-            int[][] a = new int[3][];
-            a[0] = new int[3];
-            a[1] = new int[5];
-            a[2] = new int[2];
+            bool success =  list.Remove(2);
 
-            a[0][0] = 1;
+            list.RemoveAt(0);
+            list.Clear();
 
-             
+            for (int i = 0; i < list.Count; i++)
+                Console.WriteLine(list[i]) ;
+
+            foreach (int num in list)
+                Console.WriteLine(num);
+
         }
     }
 }
